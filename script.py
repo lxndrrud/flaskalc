c645ff45d0ad67ba809d4d1084a889178a2ce52c
lxndrrud <lxndrrud@gmail.com>
2021-02-18 20:49:24 +0300

    +

--- script.py
+++ script.py
@@ -22,6 +22,9 @@ class Post(db.Model):
     text = db.Column(db.String(500), nullable=False)
     datetime = db.Column(db.DateTime, nullable=False, default=datetime.now())
     author_nickname = db.Column(db.String(50), db.ForeignKey('user.nickname'))
+    commentaries = db.relationship('Commentary', backref=db.backref('post'))
+    likes = db.relationship('Like', backref=db.backref('post'))
+    
 
     def __init__(self, title, text, author_nickname):
         self.title = title
@@ -30,7 +33,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.id}, {self.author_nickname}>'
-        
+
+
 
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -52,6 +56,7 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
     role_name = db.Column(db.Integer, db.ForeignKey('role.name'))
     posts = db.relationship('Post', backref=db.backref('user'))
+    likes = db.relationship('Like', backref=db.backref('user'))
 
     def __init__(self, nickname, name, password, role_name):
         self.nickname = nickname
@@ -60,8 +65,35 @@ class User(db.Model):
         self.role_name = role_name
 
     def __repr__(self):
-        return f'<User ({self.id}, {self.nickname}, {self.role_name}>'
+        return f'<User {self.id}, {self.nickname}, {self.role_name}>'
 
+
+class Commentary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    author_nickname = db.Column(db.String(50), db.ForeignKey('user.nickname'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+
+    def __init__(self, text, author_nickname, post_id):
+        self.text = text
+        self.author_nickname = author_nickname
+        self.post_id = post_id
+
+    def __repr__(self):
+        return f"<Commentary {self.id}, {self.author_nickname}, {self.post_id}>"
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_nickname = db.Column(db.String(50), db.ForeignKey('user.nickname'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    
+    def __init__(self, post_id, user_nickname):
+        self.user_nickname = user_nickname
+        self.post_id = post_id
+
+    def __repr__(self):
+        return f'<Like {self.post_id}, {self.user_nickname}>'
 
 
 @app.route('/', methods=['GET'])
@@ -69,6 +101,7 @@ def home():
     if not session.get('is_authenticated', False):
         session['is_authenticated'] = False
     posts = Post.query.order_by(Post.datetime).all()
+    posts.reverse()
     return render_template('home.html', posts=posts)
 
 
@@ -180,11 +213,55 @@ def post_delete(post_id):
 @app.route('/post/<int:post_id>/')
 def post_detail(post_id):
     post = Post.query.filter_by(id=post_id).first()
-    return render_template('posts/post_detail.html', post=post)
+    comments = post.commentaries
+    comments.reverse()
+    if session['is_authenticated']:
+        is_liked = Like.query.filter_by(user_nickname=session['nickname'], post_id = post_id).first() is not None
+    else: 
+        is_liked = False
+    likes = len(post.likes)
+    return render_template('posts/post_detail.html', post=post, comments=comments, likes=likes, is_liked=is_liked)
+
+
+@app.route('/post/<int:post_id>/like', methods=['GET'])
+def like(post_id):
+    if session['is_authenticated']:
+        like = Like(post_id, session['nickname'])
+        db.session.add(like)
+        db.session.commit()
+        return redirect(url_for('post_detail', post_id=post_id))
+
+@app.route('/post/<int:post_id>/unlike', methods=['GET'])
+def unlike(post_id):
+    if session['is_authenticated']:
+        like = Like.query.filter_by(post_id=post_id, user_nickname=session['nickname']).first()
+        db.session.delete(like)
+        db.session.commit()
+        return redirect(url_for('post_detail', post_id=post_id))
+
+
+@app.route('/post/<int:post_id>/comment/create', methods=['POST'])
+def comment_create(post_id):
+    if session['is_authenticated']:
+        comment = Commentary(request.form['comment-text'], session['nickname'], post_id)
+        db.session.add(comment)
+        db.session.commit()
+        return redirect(url_for('post_detail', post_id=post_id))
+
+@app.route('/post/<int:post_id>/comment/<int:comment_id>/delete', methods=['GET'])
+def comment_delete(post_id, comment_id):
+    if session['is_authenticated']:
+        comment = Commentary.query.filter_by(id=comment_id).first()
+        db.session.delete(comment)
+        db.session.commit()
+        return redirect(url_for('post_detail', post_id=post_id))
+
+
+
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    app.run('0.0.0.0', debug=True)
 
     
     
